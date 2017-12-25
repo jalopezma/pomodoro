c5b136b1c55e1ebd79f0c6b3a3afe2a84f65808e
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2017-12-25 23:09:46 +0100

    Add material and flex-layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
   ],
   declarations: [],
-  exports: [CommonModule, FormsModule],
+  exports: [
+    CommonModule,
+    FormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
 })
 export class SharedModule { }
